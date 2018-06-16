4754a334af400ae1c98dcf5ded145f87898eb21a
qulbbayusugara <38291215+qulbbayusugara@users.noreply.github.com>
2018-06-16 23:18:25 +0700

    Commit Librari, CSS, nbproject
    Commit Librari, CSS, nbproject

---
+++ librari/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+    $koneksi=mysql_connect("localhost","root","")or die("Koneksi Gagal" .mysql_error());
+    mysql_select_db("bukafest");
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
